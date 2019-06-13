e138b2c13bb899a70b8713d1dbe8ce1aebfc685e
huutuanhcmus <32814450+huutuanhcmus@users.noreply.github.com>
2019-06-13 07:40:25 +0700

    Create collider for terrain and layer class for player and speed up between animator conversion
    Create collider for terrain and layer class for player and speed up between animator conversion

--- Assets/Code/PlayerController.cs
+++ Assets/Code/PlayerController.cs
@@ -8,10 +8,12 @@ public class PlayerController : NetworkBehaviour
 {
     public GameObject bullet;
     public Transform bulletspawn;
+    Animator playerAnimation;
     // Start is called before the first frame update
     void Start()
     {
         //transform.position = new Vector3(-540.13f, 14.39f, 222.06f);
+        playerAnimation = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -24,10 +26,17 @@ public class PlayerController : NetworkBehaviour
 
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
         var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
-
+        Debug.Log(z);
+        if (z >= 0.01)
+        {
+            playerAnimation.SetBool("walk", true);
+        }
+        else
+        {
+            playerAnimation.SetBool("walk", false);
+        }
         transform.Rotate(0, x, 0);
         transform.Translate(0, 0, z);
-
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Cmdfire();
