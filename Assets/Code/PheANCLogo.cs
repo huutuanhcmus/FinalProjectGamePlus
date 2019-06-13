86564aec4c9a3d8ae1f99432520d2f98dc214cd5
huutuanhcmus <32814450+huutuanhcmus@users.noreply.github.com>
2019-06-14 06:24:56 +0700

    Updated logo phe

---
+++ Assets/Code/PheANCLogo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PheANCLogo : MonoBehaviour
+{
+    public RawImage testImage;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            testImage.enabled = true;
+        }
+        if (Input.GetKeyDown(KeyCode.F2))
+        {
+            testImage.enabled = false;
+        }
+    }
+}
---
+++ Assets/Code/PheHKMLogo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PheHKMLogo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public RawImage testImage;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            testImage.enabled = false;
+        }
+        if (Input.GetKeyDown(KeyCode.F2))
+        {
+            Debug.Log("vvvvvv");
+            testImage.enabled = true;
+        }
+    }
+}
