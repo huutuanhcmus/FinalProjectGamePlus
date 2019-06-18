c3f2da59271ff8fc9a5dfc31cab5d0d3bfb116e0
huutuanhcmus <32814450+huutuanhcmus@users.noreply.github.com>
2019-06-18 11:40:50 +0700

    updated when win

--- Assets/Code/FlagNPC.cs
+++ Assets/Code/FlagNPC.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.Networking;
 
@@ -13,8 +14,17 @@ public class FlagNPC : NetworkBehaviour
     public List<Vector3> scaleANC;
     public List<GameObject> HKMFlag;
     public List<GameObject> ANCFlag;
+    public Thread countTimeThread;
+    public Thread countTotalTimeThread;
+    public GameObject obANCLogo;
+    public GameObject obHKMLogo;
     // Start is called before the first frame update
-
+    [SyncVar] public int ANCTime = 0;
+    [SyncVar] public int HKMTime = 0;
+    public bool flag = false;
+    public bool flagTotal = false;
+    public int time;
+    public int totalTime;
     public override void OnStartServer()
     {
         base.OnStartServer();
@@ -27,6 +37,90 @@ public class FlagNPC : NetworkBehaviour
         {
             ANCFlag.Add((GameObject)Instantiate(ANCFlagOb[i], ANCPos[i].position, ANCPos[i].rotation));
             NetworkServer.Spawn(ANCFlag[i]);
+        }
+        countTimeThread = new Thread(new ThreadStart(countTime));
+        countTimeThread.Start();
+        countTotalTimeThread = new Thread(new ThreadStart(countTimeTotal));
+        countTotalTimeThread.Start();
+    }
+
+    private void Update()
+    {
+        if (flag)
+        {
+            flag = false;
+            CmdPushTime();
+            HKMTime -= 4;
+            ANCTime -= 4;
+        }
+        if (flagTotal)
+        {
+            countTimeThread.Abort();
+            countTotalTimeThread.Abort();
+            flagTotal = false;
+            Debug.Log("abcdef");
+            if (HKMTime > ANCTime)
+            {
+                var temp = (GameObject)Instantiate(obHKMLogo);
+                NetworkServer.Spawn(obHKMLogo);
+            }
+            else if(ANCTime > HKMTime)
+            {
+                var temp = (GameObject)Instantiate(obANCLogo);
+                NetworkServer.Spawn(obANCLogo);
+            }
+        }
+    }
+
+    [Command]
+    void CmdPushTime()
+    {
+        foreach (GameObject NPC in Resources.FindObjectsOfTypeAll(typeof(GameObject)))
+        {
+            if (NPC.tag == "NPC")
+            {
+                Debug.Log(NPC.name);
+                if (NPC.GetComponent<NPCControler>().Phe == 1)
+                {
+                    HKMTime++;
+                }
+                else
+                {
+                    ANCTime++;
+                }
+            }
+            else if (NPC.tag == "NPCMain")
+            {
+                Debug.Log(NPC.name);
+                if (NPC.GetComponent<NPCControler>().Phe == 1)
+                {
+                    HKMTime += 3;
+                }
+                else
+                {
+                    ANCTime += 3;
+                }
+            }
+        }
+    }
+    void countTime()
+    {
+        while (true)
+        {
+            Debug.Log("------////--");
+            Thread.Sleep(time * 1000);
+            flag = true;
+        }
+    }
+
+    void countTimeTotal()
+    {
+        while (true)
+        {
+            
+            Thread.Sleep(totalTime * 1000);
+            flagTotal = true;
+            Debug.Log("------////--");
         }
     }
 }
--- Assets/Code/NPCControler.cs
+++ Assets/Code/NPCControler.cs
@@ -18,6 +18,5 @@ public class NPCControler : NetworkBehaviour
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
