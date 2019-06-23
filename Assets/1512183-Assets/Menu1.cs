51a9351c6ca8b07156675725cfaefa62d46cd6d1
= <nhoxxu197@gmail.com>
2019-06-23 22:34:23 +0700

    Update Menu

---
+++ Assets/1512183-Assets/Menu1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Menu1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SceneChange());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator SceneChange()
+    {
+        yield return new WaitForSeconds(2.0f);
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Assets/1512183-Assets/MenuFunction.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuFunction : MonoBehaviour
+{
+    public void PlayGameButton()
+    {
+        SceneManager.LoadScene(2);
+     
+    }
+    public void OptionsButton()
+    {
+        SceneManager.LoadScene("Options");
+
+    }
+    public void SupportButton()
+    {
+        SceneManager.LoadScene("Support");
+
+    }
+    public void InformationButton()
+    {
+        SceneManager.LoadScene("Info-Team");
+
+    }
+    public void ExitsGameButton()
+    {
+        Application.Quit(); 
+    }
+}
