fae710bbfc1b23c26a3ce229da418db723ee5215
huutuanhcmus <32814450+huutuanhcmus@users.noreply.github.com>
2019-06-25 20:30:33 +0700

    updated display shyokai

---
+++ Assets/Code/Information.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Information : MonoBehaviour
+{
+    public Canvas infor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        infor.enabled = false;
+    }
+
+    public void showInfor()
+    {
+        infor.enabled = !infor.enabled;
+    }
+}
